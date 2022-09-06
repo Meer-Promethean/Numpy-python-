306bb732d2cf741f32576b1c02f99d6dc810f6f9
Meer Murtaza <108681957+Meer-Promethean@users.noreply.github.com>
2022-09-06 15:12:37 +0500

    Add files via upload

---
+++ Numpy.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("meer")
+
+
+# In[2]:
+
+
+print("get start python")
+
+
+# In[1]:
+
+
+import numpy
+
+
+# In[17]:
+
+
+import numpy as np
+
+
+# In[ ]:
+
+
+
+
+
+# In[11]:
+
+
+arr = np.array([1,2])
+arr[0]
+
+
+# In[14]:
+
+
+print(arr)
+
+
+# In[12]:
+
+
+print(np. __version__)
+
+
+# In[13]:
+
+
+print(type(arr))
+
+
+# In[15]:
+
+
+my_arr = np.array([11,22,33,44]) 
+my_arr[0]
+
+
+# In[16]:
+
+
+#2D array::
+
+array2 = np.array([1, 2, 34, 5, 6])
+
+print(array2)
+
+
+# In[17]:
+
+
+#3D array
+array3 = np.array = ([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+print(array3)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[10]:
+
+
+import numpy as np
+
+a = np.array(42)
+b = np.array([1, 2, 3, 4, 5])
+c = np.array([[1, 2, 3], [4, 5, 6]])
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)
+
+
+# In[2]:
+
+
+import numpy as np
+arr = np.array([1, 2, 3, 4], ndmin=5)
+
+print(arr)
+print('number of dimensions :', arr.ndim)
+
+
+# In[ ]:
+
+
+
+
